e7a586e16e5c60da536bf7f7ae7552d496054179
SUMUKH D N <158539917+SumukhDN@users.noreply.github.com>
2024-02-06 10:42:15 +0530

    Create grep.c

---
+++ Cprograms/grep.c
@@ -0,0 +1,21 @@
+//Simulate grep command using system call
+
+#include <stdio.h>
+#include <string.h>
+void main()
+{
+    FILE *f;
+    char filename[100], pat[100], temp[100];
+    printf("Enter filename: ");
+    scanf("%s", filename);
+    printf("Enter pattern: ");
+    scanf("%s", pat);
+    f = fopen(filename, "r");
+    while (!feof(f))
+    {
+        fgets(temp, 100, f);
+        if (strstr(temp, pat))
+            printf("%s", temp);
+    }
+    fclose(f);
+}
